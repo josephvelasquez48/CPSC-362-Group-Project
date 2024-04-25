d3bb6dfc96c085bbf8131bdb7bd707bbceed8a43
TheCheatCode1 <50853914+TheCheatCode1@users.noreply.github.com>
2024-04-24 17:35:10 -0700

    added login button
    added login button

--- version 6/login.php
+++ version 6/login.php
@@ -138,6 +138,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
             <input type="password" name="password" id="password">
 
             <button>Log in</button>
+            <p>Not registered? <a href="signup.html" style="text-decoration: none;">Create an account</a></p>
         </form>
 
 
